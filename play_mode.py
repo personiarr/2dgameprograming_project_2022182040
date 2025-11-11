86cefe145d13e8ee916a4b981c6b78c074f66e6a
최현우 <namoo3020@gmail.com>
2025-11-11 12:30:18 +0900

    기본적 구조 불러오기

---
+++ play_mode.py
@@ -0,0 +1,41 @@
+import random
+from pico2d import *
+
+import game_framework
+import game_world
+
+from knight import Knight
+
+boy = None
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            boy.handle_event(event)
+
+def init():
+    main_ch = Knight()
+    game_world.add_object(main_ch, 1)
+
+def update():
+    game_world.update()
+    # boy, ball 충돌 처리
+    game_world.handle_collision()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
+
