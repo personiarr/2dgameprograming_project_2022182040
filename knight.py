4ec3ada7eb72ba233596fa03d75ac6d07bc3e11a
최현우 <namoo3020@gmail.com>
2025-11-12 17:44:51 +0900

    공격 후 움직임, 좌우로 방향 전환 부드럽게 조정

--- knight.py
+++ knight.py
@@ -2,11 +2,14 @@ from pico2d import *
 import game_framework
 import game_world
 from state_machine import StateMachine
+from types import SimpleNamespace
 
 pressed_keys = set()
 
 FRAMES_PER_ACTION = 10
 ACTION_PER_TIME = 1
+ATK_FPA = 8
+ATK_APT = 2
 MPS = 10
 PPM = 20
 PPS = MPS * PPM
@@ -29,6 +32,9 @@ def right_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
 def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+def atk_timeout(e):
+    return e[0] == 'TIMEOUT' and ((SDLK_RIGHT in pressed_keys and not (SDLK_LEFT in pressed_keys)) or (SDLK_LEFT in pressed_keys and not (SDLK_RIGHT in pressed_keys)))
+
 
 time_out = lambda e: e[0] == 'TIMEOUT'
 class Idle:
@@ -39,7 +45,7 @@ class Idle:
         self.knight.frames = animation_frames[animation_names.index('Idle')]
         self.knight.state = 'Idle'
     def exit(self,event):
-        pass
+        return True
     def do(self):
         self.knight.frame = (self.knight.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % (self.knight.frames +1)
     def draw(self):
@@ -53,12 +59,20 @@ class Walk:
         self.knight.frame = 0
         self.knight.frames = animation_frames[animation_names.index('Walk')]
         self.knight.state = 'Walk'
-        if event[0]!='TIMEOUT' and event[1].key == SDLK_RIGHT:
-            self.knight.dir = 1
-            self.knight.move_dir = 1
-        elif event[0]!='TIMEOUT' and event[1].key == SDLK_LEFT:
-            self.knight.dir = -1
-            self.knight.move_dir = -1
+        if event[0]!='TIMEOUT' and event[1].type==SDL_KEYDOWN and (event[1].key == SDLK_RIGHT or event[1].key == SDLK_LEFT):
+            if event[1].key == SDLK_RIGHT:
+                self.knight.dir = 1
+                self.knight.move_dir = 1
+            elif event[1].key == SDLK_LEFT:
+                self.knight.dir = -1
+                self.knight.move_dir = -1
+        elif event[0]!='TIMEOUT' and event[1].type==SDL_KEYUP and (event[1].key == SDLK_RIGHT or event[1].key == SDLK_LEFT):
+            if event[1].key == SDLK_RIGHT:
+                self.knight.dir = -1
+                self.knight.move_dir = -1
+            elif event[1].key == SDLK_LEFT:
+                self.knight.dir = 1
+                self.knight.move_dir = 1
         elif event[0]=='TIMEOUT':
             if self.knight.move_dir == 1:
                 self.knight.dir = 1
@@ -66,6 +80,7 @@ class Walk:
                 self.knight.dir = -1
     def exit(self,event):
         if event[1].type==SDL_KEYUP and (event[1].key==SDLK_LEFT or event[1].key==SDLK_RIGHT):self.knight.move_dir = 0
+        return True
     def do(self):
         if self.alt_state == 0 : self.knight.frame = (self.knight.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % (self.knight.frames +1)
         #움직임
@@ -73,29 +88,6 @@ class Walk:
     def draw(self):
         self.knight.image['Walk'][int(self.knight.frame)].composite_draw(0, 'h' if self.knight.dir == 1 else '', self.knight.x, self.knight.y)
 
-class Attack:
-    def __init__(self, knight):
-        self.knight = knight
-
-    def enter(self, event):
-        self.knight.frame = 0
-        self.knight.frames = animation_frames[animation_names.index('Slash')]
-        self.knight.state = 'Slash'
-        self.effect = Effect(self.knight)
-        self.alt_flag = False
-        game_world.add_object(self.effect, 2)
-    def exit(self, event):
-        pass
-    def do(self):
-        self.knight.frame = (self.knight.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time)
-        if self.knight.frame >= 9 and not self.alt_flag:
-            self.knight.StateMachine.handle_state_event(('TIMEOUT', None))
-        #공격 딜레이, timeout 발생
-    def draw(self):
-        self.knight.image['Slash'][int(self.knight.frame)].composite_draw(0, 'h' if self.knight.dir == 1 else '', self.knight.x, self.knight.y)
-    def attack_delay(self,e):
-        self.alt_flag = True
-        return self.knight.frame >= 6 and e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_x
 
 
 class Dash:
@@ -134,11 +126,40 @@ class jump:
         self.knight.frames = animation_frames[animation_names.index('Fall')]
         self.knight.state = 'Fall'
     def exit(self, event):
-        pass
+        return True
     def do(self):
         self.knight.frame = (self.knight.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % (self.knight.frames +1)
     def draw(self):
         self.knight.image['Fall'][int(self.knight.frame)].composite_draw(0, 'h' if self.knight.dir == 1 else '', self.knight.x, self.knight.y)
+
+
+class Attack:
+    def __init__(self, knight):
+        self.knight = knight
+
+    def enter(self, event):
+        self.knight.frame = 0
+        self.knight.frames = animation_frames[animation_names.index('Slash')]
+        self.knight.state = 'Slash'
+        self.effect = Effect(self.knight)
+        self.alt_flag = False
+        self.walk_flag = False
+        game_world.add_object(self.effect, 2)
+    def exit(self, event):
+        pass
+    def do(self):
+        self.knight.frame = (self.knight.frame + ATK_FPA * ATK_APT * game_framework.frame_time)
+        if self.knight.frame >= 8 and not self.alt_flag:
+            self.knight.StateMachine.handle_state_event(('TIMEOUT', None))
+        elif self.knight.frame >= 8 and self.alt_flag:
+            self.knight.StateMachine.handle_state_event(('INPUT', SimpleNamespace(type=SDL_KEYDOWN, key=SDLK_x)))
+        #공격 딜레이, timeout 발생
+    def draw(self):
+        self.knight.image['Slash'][int(self.knight.frame)].composite_draw(0, 'h' if self.knight.dir == 1 else '', self.knight.x, self.knight.y)
+    def attack_delay(self,e):
+        if e[0]!='INPUT': return False
+        if e[1].type==SDL_KEYDOWN and e[1].key == SDLK_x:self.alt_flag = True
+        return self.knight.frame >= 6 and e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_x
 
 
 class AltAttack:
@@ -150,15 +171,20 @@ class AltAttack:
         self.knight.state = 'SlashAlt'
         self.effect = Effect(self.knight, shape='SlashEffectAlt')
         game_world.add_object(self.effect, 2)
+        self.alt_flag = False
     def exit(self, event):
         pass
     def do(self):
-        self.knight.frame = (self.knight.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % (self.knight.frames +1)
-        if self.knight.frame >= 9:
+        self.knight.frame = (self.knight.frame + ATK_FPA * ATK_APT * game_framework.frame_time) % (self.knight.frames +1)
+        if self.knight.frame >= 8 and not self.alt_flag:
             self.knight.StateMachine.handle_state_event(('TIMEOUT', None))
+        elif self.knight.frame >= 8 and self.alt_flag:
+            self.knight.StateMachine.handle_state_event(('INPUT', SimpleNamespace(type=SDL_KEYDOWN, key=SDLK_x)))
     def draw(self):
         self.knight.image['Slash'][int(self.knight.frame)].composite_draw(0, 'h' if self.knight.dir == 1 else '', self.knight.x, self.knight.y)
     def attack_delay(self,e):
+        if e[0] != 'INPUT': return False
+        if e[1].type==SDL_KEYDOWN and e[1].key == SDLK_x:self.alt_flag = True
         return self.knight.frame >= 6 and e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_x
 
 
@@ -170,7 +196,7 @@ class Effect:
         else : self.shape = shape
         self.frames = animation_frames[animation_names.index(self.shape)]
     def update(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time)
+        self.frame = (self.frame + ATK_FPA * ATK_APT * game_framework.frame_time)
         if self.frame >= self.frames:
             game_world.remove_object(self)
     def draw(self):
@@ -196,12 +222,12 @@ class Knight:
         self.StateMachine = StateMachine(
             self.IDLE,
     {
-                self.IDLE : {x_down: self.ATTACK, right_down: self.WALK, left_down: self.WALK, c_down : self.DASH, alt_down : self.JUMP},
-                self.ATTACK : {self.ATTACK.attack_delay : self.ALTATTACK, time_out : self.IDLE},
+                self.IDLE : {x_down: self.ATTACK, right_down: self.WALK, left_down: self.WALK, c_down : self.DASH, alt_down : self.JUMP, right_up : self.WALK, left_up : self.WALK},
+                self.ATTACK : {self.ATTACK.attack_delay : self.ALTATTACK, atk_timeout : self.WALK, time_out : self.IDLE, },
                 self.WALK : {x_down: self.ATTACK, time_out : self.IDLE, c_down : self.DASH, alt_down : self.JUMP, right_down : self.IDLE, left_down : self.IDLE, right_up : self.IDLE, left_up : self.IDLE},
                 self.DASH : { self.dash_timeout_to_walk: self.WALK,time_out: self.IDLE,},
                 self.JUMP : {time_out: self.IDLE},
-                self.ALTATTACK : {time_out: self.IDLE, self.ALTATTACK.attack_delay : self.ATTACK},
+                self.ALTATTACK : {self.ALTATTACK.attack_delay : self.ATTACK,atk_timeout : self.WALK, time_out: self.IDLE,},
 
     }
         )
